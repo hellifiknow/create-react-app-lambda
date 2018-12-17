e8d201c62bdb72a7e18eb4b1d73830b769bece07
Jeff <liftorgohome@gmail.com>
2018-12-17 15:14:24 -0500

    forgot to add spread operator

--- src/lambda/autocrop.js
+++ src/lambda/autocrop.js
@@ -11,7 +11,7 @@ exports.handler = function(event, context, callback) {
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Auto Cropped!', croppedRectangle })
+    body: JSON.stringify({ msg: 'Auto Cropped!', ...croppedRectangle })
   });
 
 }
